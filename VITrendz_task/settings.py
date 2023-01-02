2a9202fe6496bfced7f1440da2703a54b29f07f3
kairavsheth <85606266+kairavsheth@users.noreply.github.com>
2023-01-02 07:59:28 +0530

    Removal of Frontend and admin

--- VITrendz_task/settings.py
+++ VITrendz_task/settings.py
@@ -29,14 +29,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'frontend',
     'profile_app',
 ]
 
