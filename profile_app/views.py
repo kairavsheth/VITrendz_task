4c0ebfb65f314ba72aefbecc0848b804c1a3a3f5
kairavsheth <85606266+kairavsheth@users.noreply.github.com>
2023-01-02 08:08:43 +0530

    Fields and error handling

--- profile_app/views.py
+++ profile_app/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from profile_app.models import Profile
 from profile_app.serializers import ProfileSerializer
@@ -12,10 +13,17 @@ class ProfileViewSet(viewsets.ModelViewSet):
         queryset = Profile.objects.all()
         sort_age = self.request.query_params.get('sort_age', None)
         gender = self.request.query_params.get('gender', None)
+        fields = self.request.query_params.get('fields', None)
 
-        if gender is not None:
-            queryset = queryset.filter(gender=gender)
-        if sort_age is not None:
-            queryset = queryset.order_by('age')
-        return queryset.all()
+        try:
+            if gender is not None:
+                queryset = queryset.filter(gender=gender)
+            if sort_age is not None:
+                queryset = queryset.order_by('age')
+            if fields is not None:
+                return queryset.only(*eval(fields))
+            return queryset.all()
+        except:
+            return Response('Invalid query params', status=status.HTTP_400_BAD_REQUEST)
+
     serializer_class = ProfileSerializer
